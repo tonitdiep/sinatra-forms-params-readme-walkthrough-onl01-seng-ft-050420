07d84f419c65cb2108020bfa1d9bdd1aaf5736d2
Toni  Diep <adiep123@gmail.com>
2020-06-22 20:06:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,12 +9,12 @@ class App < Sinatra::Base
   # Add your post route and action below
   
   post '/food' do 
-    params[:favorite_food]
-    params = {
-  :name => "Sam",
-  :favorite_food => "Green Eggs and Ham"
-}
-    params.to_s
+#     params[:favorite_food]
+#     params = {
+#   :name => "Sam",
+#   :favorite_food => "Green Eggs and Ham"
+# }
+#     params.to_s
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
   end
 end
